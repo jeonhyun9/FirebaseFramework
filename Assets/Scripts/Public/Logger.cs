1b98a861759f661d6ee67bf9086fe9d24f0fbbdf
전현구 <jhg0675@nerdystar.io>
2023-08-17 11:34:47 +0900

    Logger 개선 => 컴파일 조건 체크

--- Assets/Scripts/Public/Logger.cs
+++ Assets/Scripts/Public/Logger.cs
@@ -5,37 +5,51 @@ public static class Logger
     private static readonly string separator = new('-', 50);
     public static void Log(string message, bool usePrefix = false)
     {
-        Debug.Log($"{(usePrefix ? "[Log] " : string.Empty)}{message}");
+        DebugLog($"{(usePrefix ? "[Log] " : string.Empty)}{message}");
     }
 
     public static void Success(string message)
     {
-        Debug.Log($"<color=cyan>[Success] {message}</color>");
+        DebugLog($"<color=cyan>[Success] {message}</color>");
     }
 
     public static void Warning(string message)
     {
-        Debug.Log($"<color=yellow>[Warning] {message}</color>");
+        DebugLog($"<color=yellow>[Warning] {message}</color>");
     }
 
     public static void Exception(string message, System.Exception e)
     {
         Error(message);
-        Debug.LogError($"[Exception] {e}");
+        DebugLogError($"[Exception] {e}");
     }
 
     public static void Null(string nullObjectName)
     {
-        Debug.LogError($"[Null] {nullObjectName} is null");
+        DebugLogError($"[Null] {nullObjectName} is null");
     }
 
     public static void Error(string message)
     {
-        Debug.LogError($"[Error] {message}");
+        DebugLogError($"[Error] {message}");
     }
 
     public static void Separator()
     {
-        Debug.Log(separator);
+        DebugLog(separator);
+    }
+
+    private static void DebugLog(string message)
+    {
+#if DEBUG_LOG || UNITY_EDITOR
+        Debug.Log(message);
+#endif
+    }
+
+    private static void DebugLogError(string message)
+    {
+#if DEBUG_LOG || UNITY_EDITOR
+        Debug.LogError(message);
+#endif
     }
 }
